2d1affd03639f6487cb12ea5dd6229a63e4bf675
Bethany Paul <123354450+Bethanypaul989@users.noreply.github.com>
2023-11-22 23:39:56 -0800

    Create game.rb

---
+++ game.rb
@@ -0,0 +1,22 @@
+require_relative 'player'
+
+class Game
+  def initialize(player)
+    @player = player
+  end
+
+  def increase_score
+    @player.score += 1
+    return @player.score
+  end
+end
+
+# # Test cases
+# player = Player.new("Alice", 0, 3)
+# game = Game.new(player)
+# puts "Player lives before decrease: #{player.get_lives}"
+# game.decrease_lives
+# puts "Player lives after decrease: #{player.get_lives}"
+# puts "Player score before increase: #{player.get_score}"
+# game.increase_score
+# puts "Player score after increase: #{player.get_score}"
